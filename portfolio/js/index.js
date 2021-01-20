55afb81dec242cd613bede90f914b16d9fa3d3c8
dommentee <chuku0929@gmail.com>
2021-01-20 03:11:16 -0500

    fisnished lading and hero layout

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -9,6 +9,10 @@ var curserFlicker = setInterval(flicker, 560);
 var newLineCharacter = '|';
 var text = document.createTextNode('');
 document.getElementById('greeting-wrap').appendChild(text);
+var introWrap = document.getElementById('intro-wrap');
+var showIntro = function () {
+    introWrap.classList.add('show');
+};
 var printGreeting = function (str) {
     var i = 0;
     (function getLetters() {
@@ -16,10 +20,16 @@ var printGreeting = function (str) {
         text.nodeValue += char == newLineCharacter ? '\n' : char;
         if (i < str.length) {
             setTimeout(getLetters, 200);
+            introWrap.classList.add('hide');
+        }
+        else {
+            cursor.classList.add('display-none');
+            setTimeout(showIntro, 800);
+            // introWrap.classList.add('show')
         }
     })();
 };
-printGreeting("Hello world, I'm Domacor Mentee. ");
+printGreeting("Hello world, I'm Domacor Mentee.");
 // printGreeting
 //   ("Hello world, I'm Domacor Mentee. | I'm a Front-end front end Developer. ")
 // ;
--- portfolio/js/index.ts
+++ portfolio/js/index.ts
@@ -5,32 +5,40 @@ const flicker = () => {
   cursor.classList.toggle('flicker-out');
 }
 const curserFlicker = setInterval(flicker, 560);
-
 //textnode
 const newLineCharacter = '|';
 const text = document.createTextNode('');
 document.getElementById('greeting-wrap').appendChild(text);
 
+const introWrap: any = document.getElementById('intro-wrap');
+const showIntro = () => {
+  introWrap.classList.add('show')
+} 
+
 const printGreeting = (str) => {
-  let i: number = 0;
+  let i: any = 0;
   (function getLetters() {
     const char = str[i++];
     text.nodeValue += char == newLineCharacter ? '\n' : char;
     if (i < str.length) {
-      setTimeout(getLetters, 200)
+      setTimeout(getLetters, 200);
+      introWrap.classList.add('hide')
+    } else {
+      cursor.classList.add('display-none')
+      setTimeout(showIntro, 800)
+      // introWrap.classList.add('show')
     }
-
   })();
-
-}
-
+  
+};
 printGreeting
-  ("Hello world, I'm Domacor Mentee. ")
+  ("Hello world, I'm Domacor Mentee.")
 ;
 
 // printGreeting
 //   ("Hello world, I'm Domacor Mentee. | I'm a Front-end front end Developer. ")
 // ;
+
 
 
 
