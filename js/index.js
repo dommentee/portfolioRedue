3729ab97f71615de519b455202184b23713a0d4d
dommentee <chuku0929@gmail.com>
2021-03-24 19:27:00 -0400

    moved to root

---
+++ js/index.js
@@ -0,0 +1,69 @@
+//mobile nav
+//nav in header for toggle and display nav
+var burgerMenue = document.querySelector('.uinav');
+var nav = document.getElementsByClassName('mobile-nav');
+var aboutLink = document.querySelector('.about-link');
+var portfolioLink = document.querySelector('.portfolio-link');
+var contactLink = document.querySelector('.contact-link');
+function openNav() {
+    if (nav[0].classList) {
+        nav[0].classList.toggle('closeNav');
+    }
+    else {
+        //IE9
+        var classes = nav[0].className.split(" ");
+        var i = classes.indexOf('closeNav');
+        if (i >= 0)
+            classes.splice(i, 1);
+        else
+            classes.push('closeNav');
+        nav[0].className = classes.join(" ");
+    }
+    //nav[0].classList.toggle('closeNav')
+    document.getElementById('barone').classList.toggle('disapear');
+    document.getElementById('bartwo').classList.toggle('reverse');
+    document.getElementById('barthree').classList.toggle('reversetwo');
+}
+;
+burgerMenue.addEventListener('click', openNav);
+aboutLink.addEventListener('click', openNav);
+portfolioLink.addEventListener('click', openNav);
+contactLink.addEventListener('click', openNav);
+//infninite rotation of carousel
+//inner hero varoables
+var cursor = document.getElementById('cursor');
+//cursuer flicker
+var flicker = function () {
+    cursor.classList.toggle('flicker-out');
+};
+var curserFlicker = setInterval(flicker, 560);
+//textnode
+var newLineCharacter = '|';
+var text = document.createTextNode('');
+document.getElementById('greeting-wrap').appendChild(text);
+var introWrap = document.getElementById('intro-wrap');
+var showIntro = function () {
+    introWrap.classList.add('show');
+};
+var hideCursor = function () {
+    cursor.classList.add('hide');
+};
+var printGreeting = function (str) {
+    var i = 0;
+    (function getLetters() {
+        var char = str[i++];
+        text.nodeValue += char == newLineCharacter ? '\n' : char;
+        if (i < str.length) {
+            setTimeout(getLetters, 68);
+            introWrap.classList.add('hide');
+        }
+        else {
+            setTimeout(hideCursor, 600);
+            setTimeout(showIntro, 900);
+        }
+    })();
+};
+printGreeting("Hello world, I'm Domacor Mentee.");
+// printGreeting
+//   ("Hello world, I'm Domacor Mentee. | I'm a Front-end front end Developer. ")
+// ;
---
+++ js/index.ts
@@ -0,0 +1,104 @@
+//mobile nav
+//nav in header for toggle and display nav
+const burgerMenue: any = document.querySelector('.uinav');
+const nav: any = document.getElementsByClassName('mobile-nav');
+const aboutLink: any = document.querySelector('.about-link');
+const portfolioLink: any = document.querySelector('.portfolio-link');
+const contactLink: any = document.querySelector('.contact-link');
+
+function openNav() {
+  if (nav[0].classList) {
+    nav[0].classList.toggle('closeNav')
+
+  }else {
+    //IE9
+    const classes = nav[0].className.split(" ");
+    const i = classes.indexOf('closeNav');
+
+    if (i >= 0)
+      classes.splice(i, 1);
+    else
+      classes.push('closeNav');
+      nav[0].className = classes.join(" ")
+  }
+  //nav[0].classList.toggle('closeNav')
+  document.getElementById('barone').classList.toggle('disapear');
+  document.getElementById('bartwo').classList.toggle('reverse');
+  document.getElementById('barthree').classList.toggle('reversetwo');
+};
+burgerMenue.addEventListener('click', openNav);
+aboutLink.addEventListener('click', openNav);
+portfolioLink.addEventListener('click', openNav);
+contactLink.addEventListener('click', openNav);
+
+//infninite rotation of carousel
+
+//inner hero varoables
+const cursor: HTMLElement = document.getElementById('cursor');
+//cursuer flicker
+const flicker = () => {
+  cursor.classList.toggle('flicker-out');
+}
+const curserFlicker = setInterval(flicker, 560);
+//textnode
+const newLineCharacter = '|';
+const text = document.createTextNode('');
+document.getElementById('greeting-wrap').appendChild(text);
+
+const introWrap: any = document.getElementById('intro-wrap');
+const showIntro = () => {
+  introWrap.classList.add('show')
+} 
+const hideCursor = () => {
+  cursor.classList.add('hide')
+}
+
+const printGreeting = (str) => {
+  let i: any = 0;
+  (function getLetters() {
+    const char = str[i++];
+    text.nodeValue += char == newLineCharacter ? '\n' : char;
+    if (i < str.length) {
+      setTimeout(getLetters, 68);
+      introWrap.classList.add('hide')
+    } else {
+      setTimeout(hideCursor, 600)
+      setTimeout(showIntro, 900)
+    }
+  })();
+  
+};
+printGreeting
+  ("Hello world, I'm Domacor Mentee.")
+;
+
+// printGreeting
+//   ("Hello world, I'm Domacor Mentee. | I'm a Front-end front end Developer. ")
+// ;
+
+//form redirect
+var form: any = document.getElementById("my-form");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    status.innerHTML = "Thanks for your submission!";
+    form.reset()
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+form.addEventListener("submit", handleSubmit)
+
+
+
+
+
